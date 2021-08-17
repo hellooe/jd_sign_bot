8a34e32c2ec181c8b28181821fae0512b439d1fd
hellooe <hellooe@qq.com>
2021-08-17 23:15:47 +0800

    fixes

--- app.js
+++ app.js
@@ -53,14 +53,13 @@ async function start() {
     if (fs.existsSync(path)) {
       content = fs.readFileSync(path, "utf8");
     }  
-    let t = content.match(/【签到概览】:(.*?)【/)
-    console.log(t[1])
-    let t2 = content.match(/【签到奖励】:(.*?)【/)
-    console.log(t2[1])
-    let t3 = content.match(/【其他奖励】:(.*?)【/)
-    console.log(t3[1])
-    let msg = new Date().toLocaleDateString() + '\n' + t[1] + '\n' + t2[1] + '\n' + t3[1]
-    console.log(msg)
+    let t = content.match(/【签到概览】:  (.*?)\n/)
+    t = t ? t[1] : ''
+    let t2 = content.match(/【签到奖励】:  (.*?)\n/)
+    t2 = t2 ? t2[1] : ''
+    let t3 = content.match(/【其他奖励】:  (.*?)\n/)
+    t3 = t3 ? t3[1] : ''
+    let msg = new Date().toLocaleDateString() + '\n' + t + '\n' + t2 + '\n' + t3
     await sendNotify(msg);
   }
 }
