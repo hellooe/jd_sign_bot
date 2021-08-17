1a83983bd860e3913befa8dbb7dbd0f7b8516a6a
hellooe <hellooe@qq.com>
2021-08-17 16:03:38 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const exec = require('child_process').execSync;
+const fs = require('fs');
+const rp = require('request-promise');
+const download = require('download');
+
+const KEY = process.env.JD_COOKIE;
+const serverJ = process.env.PUSH_URI;
+const sendKey = process.env.PUSH_KEY;
+
+
+async function downFile () {
+    const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
+    await download(url, './');
+}
+
+async function changeFile () {
+   let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
+   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`);
+   await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
+}
+
+async function sendNotify (content) {
+  const options ={
+    uri:  `${serverJ}`,
+    body: {
+		"sendkey": sendKey,
+		"msg_type": "text",
+		"msg": content,
+	},
+    json: true,
+    method: 'POST'
+  }
+  await rp.post(options).then(res=>{
+    console.log(res)
+  }).catch((err)=>{
+    console.log(err)
+  })
+}
+
+async function start() {
+  if (!KEY) {
+    console.log('请填写相关key')
+    return
+  }
+  await downFile();
+  console.log('代码下载完毕')
+  await changeFile();
+  console.log('变量替换完毕')
+  await exec("node JD_DailyBonus.js >> result.txt");
+  console.log('执行完毕')
+
+  if (serverJ) {
+    const path = "./result.txt";
+    let content = "";
+    if (fs.existsSync(path)) {
+      content = fs.readFileSync(path, "utf8");
+    }  
+    await sendNotify(content);
+  }
+}
+
+start()
